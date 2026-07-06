1b08906ecc560a83ead454297a1128c67a1e9015
Abdulla648 <aabdulk41@gmail.com>
2026-07-06 11:06:52 +0530

    CompanyRepository (2).java

--- CompanyRepository (2).java
+++ CompanyRepository (2).java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api") // Perfect routing
+@RequestMapping("/api") 
 public class MyController {
 
     @Autowired UserRepository userRepository;
