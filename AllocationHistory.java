e01227612f36420de940987af242970f9f2ffcb1
Abdulla648 <aabdulk41@gmail.com>
2026-07-09 09:59:22 +0530

    Update AllocationHistory.java

--- AllocationHistory.java
+++ AllocationHistory.java
@@ -12,13 +12,13 @@ public class AllocationHistory {
     
     private String studentName;
     
-    // Namma fix panna 5000 limit code ithu thaan
+    
     @Column(length = 5000)
     private String companyName;
     
     private LocalDateTime timestamp;
 
-    // Default constructor is required by JPA
+    
     public AllocationHistory() {}
 
     public AllocationHistory(String studentName, String companyName) {
@@ -27,7 +27,7 @@ public class AllocationHistory {
         this.timestamp = LocalDateTime.now();
     }
 
-    // Getters and Setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
